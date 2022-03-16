ec9a5015e2d1e7359df2cbcb5f19015f9c2e4467
nldanne <nldanne97@gmail.com>
2022-03-16 19:54:25 +0100

    created Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Created by <a href="https://uxfol.io/nldanne">NLD Anne</a></p>
+    </footer>
+  )
+}
+
+export default Footer;
